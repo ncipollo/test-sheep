3abe714e8c061acfebd43cd1fb9b194e9a8f09e7
Nick Cipollo <njc1115@gmail.com>
2025-02-17 17:35:49 -0500

    Fix test script

--- before_commit.sh
+++ before_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 uuidgen > before_commit_uuid.txt
-echo "$SHEEPIT_CURRENT_VERSION" > before_commit_versions.txt
-echo "$SHEEPIT_NEXT_VERSION" > before_commit_versions.txt
+echo "Current Version: $SHEEPIT_CURRENT_VERSION" > before_commit_versions.txt
+echo "Next Version: $SHEEPIT_NEXT_VERSION" >> before_commit_versions.txt
