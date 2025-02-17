5d81389f72c8eace7ef939509b0abba30bfbfbb6
Nick Cipollo <njc1115@gmail.com>
2025-02-17 17:33:53 -0500

    Update script

--- before_commit.sh
+++ before_commit.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-uuidgen > before_commit.txt
+uuidgen > before_commit_uuid.txt
+echo "$SHEEPIT_CURRENT_VERSION" > before_commit_versions.txt
+echo "$SHEEPIT_NEXT_VERSION" > before_commit_versions.txt
